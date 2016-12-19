5cf3ace839f9939ab5df97d9695404ca40b4872d
John Sparwasser <johnspar1@gmail.com>
2016-12-19 14:19:19 -0500

    First commit before push to github

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,23 @@
-var fs = require("fs")
-var browserify = require("browserify")
-var vueify = require("vueify")
+var gulp = require("gulp");
+var fs = require("fs");
+var browserify = require("browserify");
+var vueify = require("vueify");
+var sass = require("gulp-sass");
 
-browserify("./assets/vue/app.js")
-    .transform(vueify)
-    .bundle()
-    .pipe(fs.createWriteStream("./assets/js/compiled.js"))
+gulp.task('vue', function() {
+    return browserify("./assets/vue/app.js")
+        .transform(vueify)
+        .bundle()
+        .pipe(fs.createWriteStream("./assets/compiled/js/script.js"))
+});
+
+gulp.task('sass', function() {
+    return gulp.src('./assets/sass/includes.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./assets/compiled/css'));
+});
+
+gulp.task('default', ['vue', 'sass'], function() {
+    gulp.watch('./assets/vue/*.js', ['vue']);
+    gulp.watch('./assets/sass/*.scss', ['sass']);
+});
