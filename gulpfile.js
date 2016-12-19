2b33e3f372ebffbf24cca4e9eba94f10f4bcf8e8
John Sparwasser <johnspar1@gmail.com>
2016-12-19 13:26:23 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var fs = require("fs")
+var browserify = require("browserify")
+var vueify = require("vueify")
+
+browserify("./assets/vue/app.js")
+    .transform(vueify)
+    .bundle()
+    .pipe(fs.createWriteStream("./assets/js/compiled.js")
